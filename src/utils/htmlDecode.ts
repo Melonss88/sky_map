4e9b1a0e4b56cc69e83e515093b6c6df51e822ff
Melon <chiyudong@hotbit.pro>
2025-08-13 15:18:23 +0800

    压缩打包使用

--- src/utils/htmlDecode.ts
+++
@@ -1,7 +0,0 @@
-export function htmlDecode(input: string): string {
-    if (!input) return '';
-    const e = document.createElement('div');
-    e.innerHTML = input;
-    return e.childNodes[0]?.nodeValue || '';
-  }
-  
